814e0f1bb8afdfb96e5ebf138d9242877404a9a2
kahenya-anita <anitakahenya1@gmail.com>
2021-08-12 13:38:12 +0300

    Added urls paths and permission classes

--- courses/views.py
+++ courses/views.py
@@ -3,13 +3,15 @@ from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, \
  DeleteView
 from .models import Course
+from django.contrib.auth.mixins import LoginRequiredMixin, \
+                                        PermissionRequiredMixin
 
 
 class OwnerMixin(object):
     def get_queryset(self):
         qs = super().get_queryset()
         return qs.filter(owner=self.request.user)
-        
+
 class OwnerEditMixin(object):
     def form_valid(self, form):
         form.instance.owner = self.request.user
@@ -25,12 +27,20 @@ class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
 
 class ManageCourseListView(OwnerCourseMixin, ListView):
     template_name = 'courses/manage/course/list.html'
+    permission_required = 'courses.view_course'
 
 class CourseCreateView(OwnerCourseEditMixin, CreateView):
-    pass
+    permission_required = 'courses.add_course'
 
 class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
-    pass
+     permission_required = 'courses.change_course'
 
 class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = 'courses/manage/course/delete.html'
+    permission_required = 'courses.delete_course'
+
+class OwnerCourseMixin(OwnerMixin,
+                        LoginRequiredMixin,
+                        PermissionRequiredMixin):
+    model = Coursefields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
