54bbb685c88f99b7b4b9cbbdb6bd6c3c305895d8
kahenya-anita <anitakahenya1@gmail.com>
2021-08-12 18:19:28 +0300

    Build up the app to take user data using a multipart form

--- courses/urls.py
+++ courses/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from django.urls.resolvers import URLPattern
+from django.urls import path
 from . import views 
 
-URLPatterns = [
+urlpatterns = [
     path('mine/', views.ManageCourseListView.as_view(), name='manage_course_list'),
     path('create/', views.CourseCreateView.as_view(), name='course_create'),
     path('<pk>/edit/',views.CourseUpdateView.as_view(), name='course_edit'),
     path('<pk>/delete/',views.CourseDeleteView.as_view(), name='course_delete'),
+    path('<pk>/module/',views.CourseModuleUpdateView.as_view(),name='course_module_update'),
+    path('module/<int:module_id>/content/<model_name>/create/',views.ContentCreateUpdateView.as_view(),name='module_content_create'),
+    path('module/<int:module_id>/content/<model_name>/<id>/',views.ContentCreateUpdateView.as_view(),name='module_content_update'),
 ]
